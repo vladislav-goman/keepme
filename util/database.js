dd9dfc6e492ff31afaa6eceffe139a0927243757
Vladislav <vlad.mangoman@gmail.com>
2020-04-30 21:22:58 +0300

    DB fix.

--- util/database.js
+++ util/database.js
@@ -1,6 +1,15 @@
 const Sequelize = require("sequelize");
-const config = require('./config.json');
 
-const sequelize = new Sequelize(config.production);
+const config = process.env.DATABASE_URL
+  ? process.env.DATABASE_URL
+  : (process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+      dialect: process.env.DIALECT,
+      host: process.env.HOST,
+    });
+
+const sequelize = new Sequelize(config);
 
 module.exports = sequelize;
